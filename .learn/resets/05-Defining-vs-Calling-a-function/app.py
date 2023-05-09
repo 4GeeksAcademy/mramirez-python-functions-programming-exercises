e8e4fd8614c7cfc645e097454bdfb8fb6718736b
Miguel Ramirez <121323094+mramirez092@users.noreply.github.com>
2023-05-09 01:11:50 +0000

    OK

--- .learn/resets/05-Defining-vs-Calling-a-function/app.py
+++ .learn/resets/05-Defining-vs-Calling-a-function/app.py
@@ -1,7 +1,5 @@
 # Define the function called "multi" that expects 2 parameters:
-def multi(num1,  num2):
-    multiplication = num1 * num2
-    return multiplication
+
 # don't edit anything below this line
 return_value = multi(7,53812212)
 print(return_value)
--- .learn/resets/08-Function-that-returns/app.py
+++ .learn/resets/08-Function-that-returns/app.py
@@ -5,9 +5,3 @@ def euro_to_yen(euro_value):
 	return euro_value * 124.15
 
 ####### ↓ YOUR CODE BELOW ↓ #######
-
-dollars = 137
-euros = dollar_to_euro(137)
-yens = euro_to_yen(euros)
-
-print(yens)
